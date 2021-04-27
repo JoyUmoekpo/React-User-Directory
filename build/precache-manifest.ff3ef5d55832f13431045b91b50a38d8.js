8a89cd091800880d5d48ad126648b47988ea8319
Joy <joyumoekpo1@gmail.com>
2021-04-27 06:27:15 -0500

    Published

---
+++ build/precache-manifest.ff3ef5d55832f13431045b91b50a38d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b5e448a9d5dcfa08fc46b7359ee45c",
+    "url": "/React-User-Directory/index.html"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/React-User-Directory/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "eef5c4c34ba56f94f03b",
+    "url": "/React-User-Directory/static/css/main.3ae1f97e.chunk.css"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/React-User-Directory/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "eef5c4c34ba56f94f03b",
+    "url": "/React-User-Directory/static/js/main.de13892c.chunk.js"
+  },
+  {
+    "revision": "75117b3cc229e6d78a8d",
+    "url": "/React-User-Directory/static/js/runtime~main.0049b58b.js"
+  }
+]);
